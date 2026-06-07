5b92c1ed68cf0fceda2ccc9624cf7d0e9a5350d4
Nownitya Sharma <nownitya@gmail.com>
2026-06-07 12:05:11 +0530

    Initial commit.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 android {
@@ -43,14 +45,39 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
+    //  Androidx Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
+
+    //  Compose BOM
+    implementation(platform(libs.androidx.compose.bom))
+
+    //  Compose UI
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //  Material 3
+    implementation(libs.androidx.compose.material3)
+
+    //  Window size class
+    implementation(libs.material.window.size)
+
+    // Material icons
+    implementation(libs.androidx.material.icon.core)
+    implementation(libs.androidx.material.icon.extender)
+
+    // Navigation 3
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.androidx.material3.adaptive.navigation3)
+
+    //  Kotlin Serialization
+    implementation(libs.kotlinx.serialization.core)
+
+    // Unit Testing
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
